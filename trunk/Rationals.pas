94048ad4f84f322924dfec9558bfb49de6b92518
abreslav <abreslav@57c50fae-9619-11dd-a6b3-819ce331899f>
2008-11-20 06:59:30 +0000

    Операции с рациональными числами

--- trunk/Rationals.pas
+++ trunk/Rationals.pas
@@ -26,26 +26,104 @@ procedure subtract(var result : TRationalNumber; const a, b : TRationalNumber);
 procedure mult(var result : TRationalNumber; const a, b : TRationalNumber);
 procedure divide(var result : TRationalNumber; const a, b : TRationalNumber);
 
+
+
+
 implementation
 
-procedure add(var result : TRationalNumber; const a, b : TRationalNumber);
-begin
+var
+  tempNum1, tempNum2 : TNaturalNumber;
+  signNum : TnumberSign = nsPlus;
 
+
+
+
+procedure add(var result : TRationalNumber; const a, b : TRationalNumber);
+
+
+begin
+  naturals.mult(tempNum1, a.Numerator, b.Denominator);
+  naturals.mult(tempnum2, a.Denominator, b.Denominator);
+  case a.sign of
+    nsPlus:
+    case b.sign of
+      nsPlus: naturals.add(result.Numerator, tempNum1, tempNum2);
+      nsMinus: naturals.subtract(result.Numerator, signNum, tempNum1, tempNum2);
+    end;
+    nsMinus: begin
+      case b.sign of
+        nsPlus: naturals.subtract(result.Numerator, signNum, tempNum1, tempNum2);
+        nsMinus: naturals.add(result.Numerator, tempNum1, tempNum2);
+      end;
+      if signNum = nsPlus then
+        signNum := nsMinus
+      else
+        signNum := nsPlus;
+    end;
+  end;
+  naturals.mult(result.Denominator, a.Denominator, b.Denominator);
+  naturals.gcd(tempNum1, result.Denominator, result.Numerator);
+  naturals.divide(result.Denominator, tempNum2, result.Denominator, tempNum1);
+  naturals.divide(result.Numerator, tempNum2, result.Numerator, tempNum1);
+  result.sign := signNum;
 end;
+
+
+
 
 procedure subtract(var result : TRationalNumber; const a, b : TRationalNumber);
 begin
+  naturals.mult(tempNum1, a.Numerator, b.Denominator);
+  naturals.mult(tempnum2, a.Denominator, b.Denominator);
+  case a.sign of
+    nsPlus:
+    case b.sign of
+      nsMinus: naturals.add(result.Numerator, tempNum1, tempNum2);
+      nsPlus: naturals.subtract(result.Numerator, signNum, tempNum1, tempNum2);
+    end;
+    nsMinus: begin
+      case b.sign of
+        nsMinus: naturals.subtract(result.Numerator, signNum, tempNum1, tempNum2);
+        nsPlus: naturals.add(result.Numerator, tempNum1, tempNum2);
+      end;
+      if signNum = nsPlus then
+        signNum := nsMinus
+      else
+        signNum := nsPlus;
+    end;
+  end;
+  naturals.mult(result.Denominator, a.Denominator, b.Denominator);
+  naturals.gcd(tempNum1, result.Denominator, result.Numerator);
+  naturals.divide(result.Denominator, tempNum2, result.Denominator, tempNum1);
+  naturals.divide(result.Numerator, tempNum2, result.Numerator, tempNum1);
+  result.sign := signNum;
 
 end;
 
 procedure mult(var result : TRationalNumber; const a, b : TRationalNumber);
 begin
-
+  if (a.sign = nsPlus) xor (a.sign = nsPlus) then
+    result.sign := nsMinus
+  else
+    result.sign := nsPlus;
+  naturals.mult(result.Numerator, a.Numerator, b.Numerator);
+  naturals.mult(result.Denominator, a.Denominator, b.Denominator);
+  naturals.gcd(tempNum1, result.Denominator, result.Numerator);
+  naturals.divide(result.Denominator, tempNum2, result.Denominator, tempNum1);
+  naturals.divide(result.Numerator, tempNum2, result.Numerator, tempNum1);
 end;
 
 procedure divide(var result : TRationalNumber; const a, b : TRationalNumber);
 begin
-
+  if (a.sign = nsPlus) xor (a.sign = nsPlus) then
+    result.sign := nsMinus
+  else
+    result.sign := nsPlus;
+  naturals.mult(result.Numerator, a.Numerator, b.Denominator);
+  naturals.mult(result.Denominator, a.Denominator, b.Numerator);
+  naturals.gcd(tempNum1, result.Denominator, result.Numerator);
+  naturals.divide(result.Denominator, tempNum2, result.Denominator, tempNum1);
+  naturals.divide(result.Numerator, tempNum2, result.Numerator, tempNum1);
 end;
 
 end.
