9f37885e80e2075309275a9cccca0a721d0addc3
abreslav <abreslav@57c50fae-9619-11dd-a6b3-819ce331899f>
2008-12-18 16:49:35 +0000

    Смыкалов, изменена StrToRational (не работала) добавлена WriteLnPolynom
    отформатирован код!!!!

--- trunk/TestingUtils.pas
+++ trunk/TestingUtils.pas
@@ -7,16 +7,17 @@ uses
   Polynoms,
   Rationals;
 
-function strToNatural(s : string) : TNaturalNumber; function sravneniye(a, b : TNaturalNumber) : integer;           // Выдает -1, если первый аргумент меньше, 0, если равно и 1, если больше
+function StrToNatural(s : string) : TNaturalNumber; function sravneniye(a, b : TNaturalNumber) : integer;           // Выдает -1, если первый аргумент меньше, 0, если равно и 1, если больше
 procedure WriteNumber(const a : TNaturalNumber);                // Пишет число БЕЗ writeln
-procedure writeLnNumber(const a : TNaturalNumber);              // Пишет число с переводом строчки после написания
-procedure writePolynom(const a : TPolynom);                     // Выписывает полином
+procedure WriteLnNumber(const a : TNaturalNumber);              // Пишет число с переводом строчки после написания
+procedure WritePolynom(const a : TPolynom);                     // Выписывает полином
+procedure WriteLnPolynom(const a : TPolynom);                   // Выписывает полином и переводит строку
 procedure CopyPolynom(var a : TPolynom; const b : TPolynom);    // Присваивает первому полиному значение второго
-procedure Format10to16write(const x: TNaturalNumber);           // Получает TNaturalNumber, выводит в 16ричной сис-ме счисления
+procedure Format10To16Write(const x: TNaturalNumber);           // Получает TNaturalNumber, выводит в 16ричной сис-ме счисления
 function StrToRational(const a : string) : TRationalNumber;     // Из строки делает рациональное число. Вида '-'16ричное число'/'16ричное число или без '-' в начале
 function StrToPolynom(const a : string) : TPolynom;             // Из строки вида рациональное' 'рациональное итд (рациональное как в пред. процедуре) делает многочлен начиная со старшего члена
-procedure writeratnumber(const a : TRationalNumber);            // Пишет рациональное число
-function sravneniye_polynoms(const a, b : TPolynom) : integer;  // 0 если равны и 1 в обратном случае
+procedure WriteRatNumber(const a : TRationalNumber);            // Пишет рациональное число
+function Sravneniye_Polynoms(const a, b : TPolynom) : integer;  // 0 если равны и 1 в обратном случае
 
 implementation
 procedure Format10to16write(const x: TNaturalNumber);
@@ -28,95 +29,101 @@ begin
   end;
 end;
 
-procedure writeratnumber(const a : TRationalNumber);
+procedure WriteRatNumber(const a : TRationalNumber);
 var
-  i : integer;
+  i : Integer;
 begin
-  if a.sign = NSminus then
-    write('-');
-  writenumber(a.numerator);
-  write('/');
-  writenumber(a.denominator);
+  if a.sign = nsMinus then
+    Write('-');
+  WriteNumber(a.numerator);
+  Write('/');
+  WriteNumber(a.denominator);
 end;
 
-function chartoInt(c : char) : integer;
+function CharToInt(c : Char) : Integer;
 begin
   case UpCase(c) of
-    'A'..'Z': result := ord(c) - ord('A') + 10;
-    '0'..'9': result := ord(c) - ord('0');
+    'A'..'Z': Result := ord(c) - ord('A') + 10;
+    '0'..'9': Result := ord(c) - ord('0');
     else WriteLn('Error in sample string: char ''' + c + ''' is not allowed');
   end;
 end;
 
-function strtonatural(s : string) : TNaturalNumber;
+function StrToNatural(s : string) : TNaturalNumber;
 var
-  i, j : integer;
-  w : word;
+  i, j : Integer;
+  w : Word;
 begin
-  setlength(result, length(s) div 4);
+  SetLength(Result, length(s) div 4);
   for i := 0 to (length(s) div 4) - 1 do begin
     w := 0;
     for j := 1 to 4 do begin
       w := w * 16;
-      w := w + charToInt(s[4 * i + j]);
+      w := w + CharToInt(s[4 * i + j]);
     end;
-    result[(length(s) div 4) - i - 1] := w;
+    Result[(length(s) div 4) - i - 1] := w;
   end;
 end;
 
-function sravneniye(a, b : TNaturalNumber) : integer;
+function Sravneniye(a, b : TNaturalNumber) : Integer;
 var
-  i : integer;
+  i : Integer;
 begin
   if length(a) < length(b) then
-    setlength(a, length(b));
+    SetLength(a, length(b));
   if length(b) < length(a) then
-    setlength(b, length(a));
+    SetLength(b, length(a));
   for i := (length(a) - 1) downto 0 do begin
     if a[i] > b[i] then begin
-      result := 1;
-      exit;
+      Result := 1;
+      Exit;
     end;
     if a[i] < b[i] then begin
-      result := -1;
-      exit;
+      Result := -1;
+      Exit;
     end;
   end;
-  result := 0;
+  Result := 0;
 end;
 
-procedure writenumber(const a : TNaturalNumber);
+procedure WriteNumber(const a : TNaturalNumber);
 var
-  i : integer;
+  i : Integer;
 begin
   for i := (length(a) - 1) downto 0 do begin
-    write(a[i]);
-    write(' ');
+    Write(a[i]);
+    Write(' ');
   end;
 end;
 
-procedure writelnnumber(const a : TNaturalNumber);
+procedure WriteLnNumber(const a : TNaturalNumber);
 begin
-  writenumber(a);
-  writeln;
+  WriteNumber(a);
+  WriteLn;
 end;
 
-procedure writepolynom(const a : TPolynom);
+procedure WritePolynom(const a : TPolynom);
 var
-  i : integer;
+  i : Integer;
 begin
   for i := (length(a) - 1) downto 0 do begin
-    writeratnumber(a[i]^);
-    write('* X^');
-    write(i);
+    WriteRatNumber(a[i]^);
+    Write('* X^');
+    Write(i);
     if i <> 0 then
-      write(' + ');
+      Write(' + ');
   end;
 end;
 
-procedure copypolynom(var a : TPolynom; const b : TPolynom);
+procedure WriteLnPolynom(const a : TPolynom);
+begin
+  WritePolynom(a);
+  WriteLn;
+end;
+
+procedure CopyPolynom(var a : TPolynom; const b : TPolynom);
 var
-  i : integer;
+  i : Integer;
 begin
   SetLength(a, length(b));
   for i := 0 to (length(b) - 1) do begin
@@ -126,43 +133,44 @@ end;
 
 function StrToRational(const a : string) : TRationalNumber;
 var
-  i : integer;
+  i : Integer;
   stroka, x, y : string;
-  h : char;
+  h : Char;
 begin
   if a[1] = '-' then begin
-    result.sign := NSminus;
-    setlength(stroka, length(a) - 1);
+    Result.sign := nsMinus;
+    SetLength(stroka, length(a) - 1);
     for i := 1 to length(a) - 1 do begin
       stroka[i] := a[i + 1];
     end;
   end else begin
     stroka := a;
+    Result.sign := nsPlus;
   end;
   i := 1;
-  setlength(x, length(stroka));
+  SetLength(x, length(stroka));
   while (i <= length(stroka)) and(stroka[i] <> '/') do begin
     x[i] := stroka[i];
     i := i + 1;
   end;
   i := i - 1;
-  setlength(x, i);
+  SetLength(x, i);
   i := i + 2;
-  setlength(y, length(a));
+  SetLength(y, length(a));
   while i <= length(stroka) do begin
     //h := stroka[i];
     //stroka[i] := h;
     y[i - length(x) - 1] := stroka[i];
     i := i + 1;
   end;
-  setlength(y, length(stroka) - length(x) - 1);
-  result.numerator := strtonatural(x);
-  result.denominator := strtonatural(y);
+  SetLength(y, length(stroka) - length(x) - 1);
+  Result.numerator := StrToNatural(x);
+  Result.denominator := StrToNatural(y);
 end;
 
-function strtopolynom(const a : string) : TPolynom;
+function StrToPolynom(const a : string) : TPolynom;
 var
-  i, j, length_p, deg, l_res : integer;
+  i, j, length_p, deg, l_res : Integer;
   s, p : string;
 begin
   l_res := 0;
@@ -174,7 +182,7 @@ begin
     length_p := 0;
     while (i >= 1) and (a[i] <> ' ') do begin
       s[i] := a[i];
-      Length_p := length_p + 1;
+      length_p := length_p + 1;
       i := i - 1;
     end;
     SetLength(p, length_p);
@@ -190,44 +198,44 @@ begin
   end;
 end;
 
-function varsravneniye_polynoms(var a, b : TPolynom) : integer;
+function VarSravneniye_Polynoms(var a, b : TPolynom) : Integer;
 var
-  i : integer;
+  i : Integer;
 begin
   if length(a) > length(b) then begin
     for i := (length(a) - 1) to length(b) do begin
       if (length(a[i]^.numerator) <> 1) or (a[i]^.numerator[0] <> 0) then begin
-        result := 1;
-        exit;
+        Result := 1;
+        Exit;
       end;
-      setlength(a, length(b));
+      SetLength(a, length(b));
     end;
   end;
   if length(b) > length(a) then begin
     for i := (length(b) - 1) to length(a) do begin
       if (length(b[i]^.numerator) <> 1) or (b[i]^.numerator[0] <> 0) then begin
-        result := 1;
-        exit;
+        Result := 1;
+        Exit;
       end;
-      setlength(b, length(a));
+      SetLength(b, length(a));
     end;
   end;
   for i := 0 to (length(a) - 1) do begin
     if (a[i]^.sign <> b[i]^.sign) or (a[i]^.numerator <> b[i]^.numerator) or (a[i]^.denominator <> b[i]^.denominator) then begin
-      result := 1;
-      exit;
+      Result := 1;
+      Exit;
     end;
   end;
-  result := 0;
+  Result := 0;
 end;
 
-function sravneniye_polynoms(const a, b : TPolynom) : integer;
+function Sravneniye_Polynoms(const a, b : TPolynom) : Integer;
 var
   x, y : TPolynom;
 begin
-  copypolynom(x, a);
-  copypolynom(y, b);
-  varsravneniye_polynoms(x, y);
+  CopyPolynom(x, a);
+  CopyPolynom(y, b);
+  VarSravneniye_Polynoms(x, y);
 end;
 
 end.
